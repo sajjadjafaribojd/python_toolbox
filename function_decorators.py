a158513072320f5cb196719a5bcdf60ed9e2c716
JabJ <sajjadjafaribojd@gmail.com>
2023-02-05 15:51:01 +0000

    create decorator function.

--- function_decorators.py
+++ function_decorators.py
@@ -16,3 +16,21 @@ print(site('JabJ.ir'))  #JabJ.ir
 print(welcome_message(site('JabJ.ir'))) #Welcome to JabJ.ir  
 
 
+#A decorator is a function that takes a function as its only parameter and returns a function.
+
+# Adds a welcome message to the string
+# returned by fun(). Takes fun() as
+# parameter and returns welcome().
+
+def welcome_message_0(func):
+    # Nested function
+    def add_weblcome(site_name):
+        return 'Welcome to ' + site_name
+
+    # Decorator returns a function
+    return add_weblcome    
+
+@welcome_message_0
+def site_0(site_name):
+    return site_name
+
