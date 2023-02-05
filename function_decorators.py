2a48fe1b945566f4f5ab2b20965674e479cb2243
JabJ <sajjadjafaribojd@gmail.com>
2023-02-05 14:35:31 +0000

    Call function with another function.

--- function_decorators.py
+++ function_decorators.py
@@ -1,1 +1,18 @@
+# A Python program to demonstrate that a function
+# can be defined inside another function and a
+# function can be passed as parameter.
+  
+# Adds a welcome message to the string
+def welcome_message(str):
+    def add_welcome():
+        return 'Welcome to '
+
+    return add_welcome() + str    
+
+def site(site_name):
+    return site_name
+
+print(site('JabJ.ir'))  #JabJ.ir
+print(welcome_message(site('JabJ.ir'))) #Welcome to JabJ.ir  
+
 
