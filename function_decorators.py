718afefc43720b9e47539d1f2f7ba44646fe200c
JabJ <sajjadjafaribojd@gmail.com>
2023-02-06 09:57:47 +0000

    2 decorator with diffrent priority

--- function_decorators.py
+++ function_decorators.py
@@ -100,3 +100,31 @@ def saygfg():
          
 sayhellogfg()
 saygfg()
+
+
+
+# code for testing decorator chaining
+def decor1(func):
+    def inner():
+        x = func()
+        return x * x
+    return inner
+ 
+def decor(func):
+    def inner():
+        x = func()
+        return 2 * x
+    return inner
+ 
+@decor1
+@decor
+def num():
+    return 10
+ 
+@decor
+@decor1
+def num2():
+    return 10
+   
+print(num())
+print(num2())
